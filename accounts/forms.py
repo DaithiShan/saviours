df73815f32e84ffd2c1f0203953bbeaf6e01ff71
DaithiShan <david.shanahan.burns@gmail.com>
2021-12-31 15:00:48 +0000

    Login form, view and template added

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,6 +4,9 @@ from django.contrib.auth import authenticate
 
 from .models import Account
 
+# Custom forms adapted from CodingWithMitch:
+# https://www.youtube.com/playlist?list=PLgCYzUzKIBE_dil025VAJnDjNZHHHR9mW
+
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(max_length=60)
@@ -27,3 +30,18 @@ class RegistrationForm(UserCreationForm):
             "password1",
             "password2",
         )
+
+
+class AccountAuthenticationForm(forms.ModelForm):
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = Account
+        fields = ("email", "password")
+
+    def clean(self):
+        if self.is_valid():
+            email = self.cleaned_data["email"]
+            password = self.cleaned_data["password"]
+            if not authenticate(email=email, password=password):
+                raise forms.ValidationError("Invalid email and/or password")
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import register_user
+from .views import (register_user, logout_user, login_user)
 
 urlpatterns = [
-    path("register/", register_user, name="register")
+    path("register/", register_user, name="register"),
+    path("logout/", logout_user, name="logout"),
+    path("login/", login_user, name="login")
 ]
