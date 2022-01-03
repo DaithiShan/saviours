e0e653715fb4d90f71cee92d25781f2f295e00d1
DaithiShan <david.shanahan.burns@gmail.com>
2022-01-03 13:29:25 +0000

    Toasts added for django messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
 
 from .models import Account
 from .forms import RegistrationForm, AccountAuthenticationForm
@@ -25,11 +26,11 @@ def register_user(request):
             raw_password = form.cleaned_data.get("password1")
             account = authenticate(email=email, password=raw_password)
             login(request, account)
-            # messages.success(
-            #     request,
-            #     f"Thanks for registering,\
-            #         {request.user.first_name}! You are now logged in 🙂",
-            # )
+            messages.success(
+                request,
+                f"Thanks for registering,\
+                    {request.user.first_name}! You are now logged in!",
+            )
             return redirect(redirect_url)
         else:
             context["registration_form"] = form
@@ -44,7 +45,7 @@ def logout_user(request):
     View to log users out from the website
     """
     logout(request)
-    # messages.success(request, f"You have successfully logged out 👋")
+    messages.success(request, f"{'You have successfully logged out'}")
     return redirect("/")
 
 
@@ -66,19 +67,19 @@ def login_user(request):
             user = authenticate(email=email, password=password)
             if user:
                 login(request, user)
-                # messages.success(
-                #     request, f"Welcome back, {request.user.first_name}! 🙂"
-                # )
+                messages.success(
+                    request, f"Welcome back, {request.user.first_name}!"
+                )
                 return redirect(redirect_url)
         else:
-            # messages.error(request, f"Incorrect login details")
+            messages.error(request, f"{'Incorrect login details'}")
             context["login_form"] = form
 
     else:
         form = AccountAuthenticationForm()
-        # if request.POST.get("next"):
-        #     # messages.error(
-        #     #     request, f"You must be logged in to access this page"
-        #     # )
+        if request.POST.get("next"):
+            messages.error(
+                request, f"You must be logged in to access this page"
+            )
         context["login_form"] = form
     return render(request, "accounts/login.html", context)
