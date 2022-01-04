8db79fe6ae26351242b99962cb4508f051fe414a
DaithiShan <david.shanahan.burns@gmail.com>
2022-01-04 15:05:48 +0000

    Initial fixtures added to products app

--- store/views.py
+++ store/views.py
@@ -7,30 +7,8 @@ def home(request):
 
 
 def shop(request):
-    # products = Product.objects.all()
+    products = Product.objects.all()
     context = {
-        'products': [
-            {
-                "pk": 1,
-                "title": "St Saviours OBA Hoodie",
-                "slug": "st-saviours-oba-hoodie",
-                "description": "St Saviours Hoodie in Club colors.",
-                "price": "50.00",
-            },
-            {
-                "pk": 2,
-                "title": "St Saviours OBA Tshirt",
-                "slug": "st-saviours-oba-tshirt",
-                "description": "St Saviours Tshirt in Club Colors",
-                "price": "15.00",
-            },
-            {
-                "pk": 3,
-                "title": "St Saviours OBA Sweatpants",
-                "slug": "st-saviours-oba-sweatpants",
-                "description": "St Saviours Sweatpants in Club Colors",
-                "price": "40.00",
-            }
-        ]
-    }
+        'products': products
+        }
     return render(request, 'store/shop.html', context) 
