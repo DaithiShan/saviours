b76f7bdcabe22887eadbb9ca846d8f8d4e3e32b9
DaithiShan <david.shanahan.burns@gmail.com>
2022-01-07 14:43:11 +0000

    Added product categories and subcategories to global context

---
+++ store/contexts.py
@@ -0,0 +1,17 @@
+from products.models import Product, Category
+
+def subcategories_per_category(request):
+    """
+    function to insert product categories,
+    into base template
+    """
+    context = {}
+    context["categories"] = Category.objects.all()
+    context["category_subcategories"] = {}
+    for category in context["categories"]:
+        context["category_subcategories"][category] = sorted(set(
+            Product.objects.filter(category=category).values_list(
+                "subcategory__title", flat=True
+            )
+        ))
+    return context
