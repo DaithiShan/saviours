e5f52863c16e912512fe60b8bccb6baa21558ae1
DaithiShan <david.shanahan.burns@gmail.com>
2022-01-14 22:30:29 +0000

    Bag urls, views, templates updated

---
+++ bag/templatetags/bag_tools.py
@@ -0,0 +1,12 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='calc_subtotal')
+def calc_subtotal(price, quantity):
+    """
+    Calculate the subtotal of items
+    ref Boutique Ado lesson: https://tinyurl.com/2ktsebwu
+    """
+    return price * quantity
