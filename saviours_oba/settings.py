d4550a966050ee95d07e90ad4f4a78b136159b85
DaithiShan <david.shanahan.burns@gmail.com>
2022-01-16 16:58:04 +0000

    Configured app for deployment to heroku pipeline

--- saviours_oba/settings.py
+++ saviours_oba/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ["saviours.herokuapp.com", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["saviours-testing-app.herokuapp.com","saviours-oba.herokuapp.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -106,17 +107,22 @@ WSGI_APPLICATION = 'saviours_oba.wsgi.application'
 CSRF_TRUSTED_ORIGINS = ['https://8000-turquoise-parrot-8u22qfps.ws-eu25.gitpod.io',
                         'https://8000-plum-swordfish-k5z8tzch.ws-eu25.gitpod.io',
                         'https://8000-blue-piranha-8v2xuc7b.ws-eu25.gitpod.io',
-                        'https://8000-blue-piranha-8v2xuc7b.ws-eu27.gitpod.io' ]
+                        'https://8000-blue-piranha-8v2xuc7b.ws-eu27.gitpod.io', ]
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
