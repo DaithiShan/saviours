103dba50a4824315e69f7e9783e3e2fca320c3aa
DaithiShan <david.shanahan.burns@gmail.com>
2022-01-18 19:03:18 +0000

    Updated db.sqlite after configuring for heroku, updated settings for same

--- saviours_oba/settings.py
+++ saviours_oba/settings.py
@@ -112,17 +112,17 @@ CSRF_TRUSTED_ORIGINS = ['https://8000-turquoise-parrot-8u22qfps.ws-eu25.gitpod.i
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-if "DATABASE_URL" in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# if "DATABASE_URL" in os.environ:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
