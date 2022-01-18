a2e0b78ad5de90053b926e47bdec3dea187cb999
DaithiShan <david.shanahan.burns@gmail.com>
2022-01-18 19:10:23 +0000

    Added heroku review app to allowed hosts in settings

--- saviours_oba/settings.py
+++ saviours_oba/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ["saviours-testing-app.herokuapp.com","saviours-oba.herokuapp.com", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["saviours-testing-app.herokuapp.com","saviours-oba.herokuapp.com", "127.0.0.1", "localhost", "saviours-daithishan-dev-cvappv.herokuapp.com"]
 
 
 # Application definition
