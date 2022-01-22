28a870f28edf12eb39001b68880b19019bd9daa9
DaithiShan <david.shanahan.burns@gmail.com>
2022-01-22 09:11:31 +0000

    Custom user forms added to accounts app

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
 
-from .models import Account
+from .models import Account, Address
 
 # Custom forms adapted from CodingWithMitch:
 # https://www.youtube.com/playlist?list=PLgCYzUzKIBE_dil025VAJnDjNZHHHR9mW
@@ -45,3 +45,49 @@ class AccountAuthenticationForm(forms.ModelForm):
             password = self.cleaned_data["password"]
             if not authenticate(email=email, password=password):
                 raise forms.ValidationError("Invalid email and/or password")
+
+
+class EditAccountForm(forms.ModelForm):
+    email = forms.EmailField(max_length=60)
+    newsletter = forms.TypedChoiceField(
+        coerce=lambda x: x == "True",
+        choices=(
+            (True, "Yes, subscribe me to the newsletter"),
+            (False, "No thank you"),
+        ),
+        widget=forms.RadioSelect,
+        initial=True,
+    )
+
+    class Meta:
+        model = Account
+        fields = ("first_name", "last_name", "email", "newsletter")
+
+
+class DeleteAccountForm(forms.Form):
+    password = forms.CharField(
+        required=True,
+        label="Password",
+        widget=forms.PasswordInput(
+            attrs={"placeholder": "Please confirm your password"}
+        ),
+    )
+
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = (
+            "street_address_1",
+            "street_address_2",
+            "town_or_city",
+            "county",
+            "postcode",
+            "country",
+            "phone_number",
+        )
+        labels = {
+            "street_address_1": "Address 1",
+            "street_address_2": "Address 2 (optional)",
+            "town_or_city": "Town / City",
+        }
