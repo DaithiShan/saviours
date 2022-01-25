89a947f7edce6c6fbf56c0014b1fadf83924d29b
DaithiShan <david.shanahan.burns@gmail.com>
2022-01-25 22:04:29 +0000

    Updated settigs, db.json and sqlite when configing main for heroku

--- saviours_oba/settings.py
+++ saviours_oba/settings.py
@@ -114,7 +114,8 @@ CSRF_TRUSTED_ORIGINS = ['https://8000-turquoise-parrot-8u22qfps.ws-eu25.gitpod.i
                         'https://8000-plum-swordfish-k5z8tzch.ws-eu25.gitpod.io',
                         'https://8000-blue-piranha-8v2xuc7b.ws-eu25.gitpod.io',
                         'https://8000-blue-piranha-8v2xuc7b.ws-eu27.gitpod.io',
-                        'https://8000-blue-piranha-8v2xuc7b.ws-eu28.gitpod.io']
+                        'https://8000-blue-piranha-8v2xuc7b.ws-eu28.gitpod.io',
+                        'https://8000-daithishan-saviours-mjr8wv9kzza.ws-eu29.gitpod.io']
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
