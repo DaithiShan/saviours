9d3b92f1b415f74fe1e352ccd14ba7ae9431a9c9
DaithiShan <david.shanahan.burns@gmail.com>
2022-01-29 21:31:55 +0000

    Uncached static js from AWS in attempt to update it on heroku

--- saviours_oba/settings.py
+++ saviours_oba/settings.py
@@ -187,10 +187,6 @@ if 'USE_AWS' in os.environ:
     Static files are collected in static folder in s3 bucket
     """
     # Cache control
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
-    }
 
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'saviours-oba'
